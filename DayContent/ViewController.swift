296c4d4100915abec9ca1db0f1e17264ef16d215
Tigran Simonyan (LuckyAds) <tigransimonyanluckyads@Tigrans-MacBook-Pro.local>
2020-07-07 20:50:58 +0400

    Inital commit

---
+++ DayContent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DayContent
+//
+//  Created by Tigran Simonyan (LuckyAds) on 7/7/20.
+//  Copyright © 2020 Tigran Simonyan (LuckyAds). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
